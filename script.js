8527cb442b7d27f59ea246930ca1ffeb4e11d476
Amami27 <amamitejiri@gmail.com>
2019-09-02 23:10:29 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// let jumpTo = document.querySelector("button");
-// function push(){
-//   let shoppingListName = document.querySelector("input");
-//   let shoppingList = document.createElement("shoppingList");
-//   shoppingList.appendChild(document.createTextNode(shoppingListName.value));
-//   document.querySelector("div").appendChild(shoppingList);
-//   shoppingList.appendChild(document.createTextNode("(0,0)"));
-//   document.querySelector("div").appendChild(shoppingList);
-//   shoppingListName.value = "";
-// }
-// add.addEventListener('click', push);
-// let add = document.querySelector("button");
-// function push(){
-//   let shoppingListName = document.querySelector("input");
-//   let shoppingList = document.createElement("shoppingList");
-//   shoppingList.appendChild(document.createTextNode(shoppingListName.value));
-//   document.querySelector("div").appendChild(shoppingList);
-//   shoppingList.appendChild(document.createTextNode("(0,0)"));
-//   document.querySelector("div").appendChild(shoppingList);
-//   shoppingListName.value = "";
-// }
-// add.addEventListener('click', push);
-  
-
-
 let add = document.querySelector("#addList");
 function push(){
   let shoppingListInput = document.querySelector("input");
@@ -30,10 +5,9 @@ function push(){
   shoppingList.appendChild(document.createTextNode(shoppingListInput.value));
   document.querySelector("div").appendChild(shoppingList);
   shoppingList.appendChild(document.createTextNode("(0,0)"));
-  document.querySelector("div").appendChild(shoppingList);
+  document.querySelector("div").appendChild(viewCategory());
   shoppingListInput.value = "";
   shoppingList.style.maxWidth = "700px";
   shoppingList.style.margin = "0 auto"
 }
 
-add.addEventListener('click', push);
